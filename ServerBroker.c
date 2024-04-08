cb191c2fcff017cc3ce1441db5f736e790f73687
Nicolas Betancur <nicobetancurochoa@gmail.com>
2024-04-07 21:18:43 -0500

    Server Integrations

--- ServerBroker.c
+++ ServerBroker.c
@@ -5,21 +5,16 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <pthread.h>
-//standard libraries used for programming with threads
 
 #define MAX_CLIENTES 10
 #define BUFFER_SIZE 2048  // Tamaño suficiente para la mayoría de los paquetes MQTT
-//#define PUERTO_MQTT 1883
 #define DEFAULT_PORT 1883
-// Structure to hold client information
+
+// Estructura para almacenar la información del cliente
 typedef struct {
     int sockfd;
-    struct sockaddr_in addr; //address info of the client connecting to the server
+    struct sockaddr_in addr; // Información de la dirección del cliente que se conecta al servidor
 } Cliente;
-
-Cliente clientes[MAX_CLIENTES];
-pthread_mutex_t mutexClientes = PTHREAD_MUTEX_INITIALIZER;
-
 
 void *manejarConexionCliente(void *data) {
     int sockfd = *((int*)data);
@@ -28,19 +23,59 @@ void *manejarConexionCliente(void *data) {
     char buffer[BUFFER_SIZE];
     ssize_t mensajeLen;
 
-    // Bucle de lectura: leer los datos del cliente
-    while ((mensajeLen = recv(sockfd, buffer, BUFFER_SIZE, 0)) > 0) {
-        // Aquí se manejarían los mensajes MQTT recibidos
-        //POR IMPLEMENTAR Almacenamiento de mensajes
+    // Esperar por un mensaje
+    mensajeLen = recv(sockfd, buffer, BUFFER_SIZE - 1, 0);
+    if (mensajeLen > 0) {
+        buffer[mensajeLen] = '\0';  // Asegurar que el buffer es una cadena de caracteres válida
+
+        // Deserializar el mensaje MQTT
+        char MessageType[5]; // Primeros 4 caracteres + '\0'
+        char DUPFlag[2];     // Quinto carácter + '\0'
+        char QoSFlag[3];     // Sexto y séptimo caracteres + '\0'
+        char RETAIN[2];      // Octavo carácter + '\0'
+
+        strncpy(MessageType, buffer, 4);
+        MessageType[4] = '\0';
+
+        DUPFlag[0] = buffer[4];
+        DUPFlag[1] = '\0';
+
+        strncpy(QoSFlag, buffer + 5, 2);
+        QoSFlag[2] = '\0';
+
+        RETAIN[0] = buffer[7];
+        RETAIN[1] = '\0';
+
+        // Convertir MessageType de binario a decimal
+        int messageTypeInt = strtol(MessageType, NULL, 2);
+
+        // Usar un switch para interpretar MessageType
+        switch (messageTypeInt) {
+            
+            case 1: // CONNECT
+                printf("CONNECT: Client request to connect to Server\n");
+                // Enviar respuesta CONNACK (representación simple)
+                char mensajeConnack[] = "0010xxxx";
+                if (send(sockfd, mensajeConnack, strlen(mensajeConnack), 0) < 0) {
+                    perror("Fallo al enviar mensaje CONNACK");
+                } else {
+                    printf("Mensaje CONNACK enviado al cliente.\n");
+                }
+                break;
 
 
-        printf("Mensaje recibido (bytes): %zd\n", mensajeLen);
-        // Por simplicidad, solo imprimimos la longitud del mensaje
-    }
-    if (mensajeLen == 0) {
-        // Cliente desconectado
-        printf("Cliente desconectado\n");
-    } else if (mensajeLen < 0) {
+            case 3: // PUBLISH                  //Why jumps to case 2???? REVISAR!
+
+                printf("PUBLISH: Publish message\n");
+                // Aquí se enviaría una respuesta adecuada para PUBLISH, por ejemplo, PUBACK
+                break;
+            // Agregar más casos según sea necesario
+            default:
+                printf("Mensaje no esperado o desconocido. MessageType: %d\n", messageTypeInt);
+        }
+    } else if (mensajeLen == 0) {
+        printf("Cliente desconectado antes de cualquier acción\n");
+    } else {
         perror("Error en recv");
     }
 
@@ -48,77 +83,62 @@ void *manejarConexionCliente(void *data) {
     return NULL;
 }
 
-int main(int argc, char argv[]) {
-// manejo de argumentos por consola
+int main(int argc, char *argv[]) {
 
+    // Manejo de argumentos por consola
+    
     if(argc != 4){
         fprintf(stderr, "Uso: %s <ip> <port> <path/log.log>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-
+    
     char *ip = argv[1];
     char *port = argv[2];
     char *logPath = argv[3];
 
-    // Asegurándose de que el puerto no esté vacío, si lo está, usa el puerto por defecto
-    if (strlen(port) == 0) {
-        port = DEFAULT_PORT;
-    }
-
-    // Por ahora, solo imprimimos los valores para verificar
-
-    //Server Sturcture
+    // Configuración inicial del servidor, incluyendo la creación del socket y la vinculación a la dirección IP y puerto especificados
     struct sockaddr_in servidorAddr = {
         .sin_family = AF_INET,
-        .sin_addr.s_addr = inet_addr(ip),
+        .sin_addr.s_addr = atoi(inet_addr), // Escuchar en todas las interfaces, esto hay que revisarlo mejor!
         .sin_port = htons(atoi(port)),
     };
-    printf("Iniciando el servidor en IP: %s, Puerto: %s\n", ip, port);
+    
+    printf("Iniciando el servidor en Puerto: %s\n", port);
 
-    //Creacion del socket
-    int servidorSockfd = socket(AF_INET, SOCK_STREAM, 0);  //AF_INET VERIFICAR !!!! "SOCK_STREAM IS STANDAR TCP. CONNECTION BASED PROTOCOL, SENDS VARIOUS PACKETS"
+    // Creación del socket
+    int servidorSockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (servidorSockfd < 0) {
         perror("Error al crear el socket del servidor");
         exit(EXIT_FAILURE);
     }
 
-      //bind server to socket
+    // Vincular el servidor al socket
     if (bind(servidorSockfd, (struct sockaddr *)&servidorAddr, sizeof(servidorAddr)) < 0) {
         perror("Error al vincular el socket del servidor");
         close(servidorSockfd);
         exit(EXIT_FAILURE);
     }
-    //listen for incoming connections
+
+    // Escuchar conexiones entrantes
     if (listen(servidorSockfd, MAX_CLIENTES) < 0) {
         perror("Error al escuchar en el socket del servidor");
         close(servidorSockfd);
         exit(EXIT_FAILURE);
     }
 
-    printf("Servidor MQTT ejecutándose en el puerto %s...\n", port);
+    printf("Servidor ejecutándose en el puerto %s...\n", port);
 
     // Manejo del archivo de log
-    FILE *logFile = fopen(logPath, "a"); // Abre el archivo en modo append
+    FILE *logFile = fopen(logPath, "a"); // Abrir el archivo en modo append
     if (logFile == NULL) {
         perror("Error al abrir el archivo de log");
         exit(EXIT_FAILURE);
     }
-     // Registrando el inicio del servidor en el archivo de log
     fprintf(logFile, "Servidor iniciado en IP: %s, Puerto: %s\n", ip, port);
-    fclose(logFile); 
+    fclose(logFile); // Cerrar el archivo de log
 
-/*
-old
-struct sockaddr_in server_addr;
-server_addr.sin_family = AF_INET;
-server_addr.sin_addr.s_addr = inet_addr(ip);
-server_addr.sin_port = htons(atoi(port));
-*/
-
-
-//manejo de clientes continuos, alocar espacio de memoria para los clientes entrantes, desalocar las threads para darselas a los nuevos clientes.
+    // Bucle principal del servidor para aceptar conexiones entrantes
     while (1) {
-
         struct sockaddr_in clienteAddr;
         socklen_t clienteAddrLen = sizeof(clienteAddr);
         int* nuevoSockfd = malloc(sizeof(int));
@@ -130,6 +150,7 @@ server_addr.sin_port = htons(atoi(port));
             continue;
         }
 
+        // Crear un hilo para manejar la conexión con el cliente
         pthread_t threadId;
         if (pthread_create(&threadId, NULL, manejarConexionCliente, nuevoSockfd) != 0) {
             perror("Error al crear el hilo del cliente");
